c5ef3e05e3ca0124c6b5d66fb8b146f4054c0652
mtkmk1881 <mtkmk1881@gmail.com>
2026-09-06 16:03:53 +0300

    Add dotenv support and configuration for server host and port

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use axum::{Router, routing::get};
+use dotenvy::dotenv;
+use std::env;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok(); // Dotenv install
+
+    let host: String = env::var("HOST").expect("HOST is not set in the .env file!");
+    let port: String = env::var("PORT").expect("PORT is not set in the .env file!");
+
+    let full_url: String = format!("{}:{}", &host, &port);
+
     let app: Router = Router::new().route("/", get(|| async { "Home Page" }));
-    let listener: TcpListener = TcpListener::bind("0.0.0.0:3000")
-        .await
-        .unwrap();
+    let listener: TcpListener = TcpListener::bind(&full_url).await.unwrap();
 
+    println!("Full_url:{}", &full_url);
     axum::serve(listener, app).await.unwrap();
-
-    println!("Server lsitening");
 }
