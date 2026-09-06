21dea1d689f6d1649cda0bbf550e0f2e1e684fc0
mtkmk1881 <mtkmk1881@gmail.com>
2026-09-06 19:31:45 +0300

    Add login route and update dependencies for JSON handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
-use axum::{Router, routing::get};
+use axum::{Json, Router, routing::get, routing::post};
 use dotenvy::dotenv;
+use serde_json::Value;
 use std::env;
 use tokio::net::TcpListener;
+
+async fn home_route() -> &'static str {
+    "Home Endpoint"
+}
+
+async fn login_route(Json(body): Json<Value>) -> &'static str {
+    println!("Username: {}", body["username"]);
+    "Login Endpoint"
+}
 
 #[tokio::main]
 async fn main() {
@@ -11,9 +21,11 @@ async fn main() {
     let port: String = env::var("PORT").expect("PORT is not set in the .env file!");
 
     let full_url: String = format!("{}:{}", &host, &port);
-
-    let app: Router = Router::new().route("/", get(|| async { "Home Page" }));
     let listener: TcpListener = TcpListener::bind(&full_url).await.unwrap();
+
+    let app = Router::new()
+        .route("/", get(home_route))
+        .route("/auth/login", post(login_route));
 
     println!("Full_url:{}", &full_url);
     axum::serve(listener, app).await.unwrap();
