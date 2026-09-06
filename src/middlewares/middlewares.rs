fd8f9f6a472fb2a23a853bf4ca11f2751ced3a44
mtkmk1881 <mtkmk1881@gmail.com>
2026-09-07 01:21:49 +0300

    Update logger middleware to change log message from "Req" to "Url"

--- src/middlewares/middlewares.rs
+++ src/middlewares/middlewares.rs
@@ -1,7 +1,7 @@
 use axum::{extract::Request, middleware::Next, response::Response};
 
 pub async fn logger_middleware(req: Request, next: Next) -> Response {
-    println!("Req: {:?}", req.uri());
+    println!("Url: {:?}", req.uri());
     let response = next.run(req).await;
 
     response
