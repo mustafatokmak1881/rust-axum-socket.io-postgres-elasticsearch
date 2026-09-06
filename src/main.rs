5f912f489f96004472cb5d45baf1c7126de24c24
mtkmk1881 <mtkmk1881@gmail.com>
2026-09-06 15:35:18 +0300

    Implement basic Axum server with routing and TCP listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app: Router = Router::new().route("/", get(|| async { "Home Page" }));
+    let listener: TcpListener = TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+
+    println!("Server lsitening");
 }
